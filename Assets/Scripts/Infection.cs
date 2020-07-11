2a92a36c2cb4c35e81f22b43243754f170552b63
Carter <andrewscart@gmail.com>
2020-07-11 14:34:45 -0700

    Infection

---
+++ Assets/Scripts/Infection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infection : MonoBehaviour
+{
+    public SpriteRenderer renderer;
+    public Sprite sick;
+    public Sprite healthy;
+
+    public bool infected=false;
+    public void Infect(){
+        infected=true;
+        renderer.sprite=sick;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("agent")){
+            if(collision.collider.GetComponent<Infection>().infected){
+                Infect();
+            }
+        }
+    }
+}
